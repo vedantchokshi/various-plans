e90cbc4b20e75f4da2b861a5dc849e3aef3e6328
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-21 20:01:47 +0000

    Remove run in main.py
    * use dev_appserver.py to run

--- main.py
+++ main.py
@@ -202,7 +202,3 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-
-
-if __name__ == '__main__':
-    app.run()
