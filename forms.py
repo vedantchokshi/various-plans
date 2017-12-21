47a5c5b512fd8adc37c123553c781004897bd6a8
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-21 22:33:40 +0000

    Tidy forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, SubmitField
 
+
 # TODO VERIFY ALL FIELDS ARE CORRECT TYPES AND ADD VALIDATION
 
 class PlanForm(FlaskForm):
@@ -8,11 +9,13 @@ class PlanForm(FlaskForm):
     name = TextField("Name Of Plan")
     submit = SubmitField("Create!")
 
+
 class EventForm(FlaskForm):
     # TODO TextField is deprecated
     name = TextField("Name Of Event")
     location = TextField("Location Of Event")
     submit = SubmitField("Create!")
+
 
 class RouteForm(FlaskForm):
     # TODO TextField is deprecated
