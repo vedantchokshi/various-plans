d3b0472ed318f37b1ed664dddc2fb5452ade6fd0
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-23 02:30:37 +0000

    Implement all resource GET urls
    * lots of comments to show how to deal with url arguments

--- api/locations.py
+++ api/locations.py
@@ -1,3 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-ROUTES = Blueprint('locations')
+ROUTES = Blueprint('locations', __name__)
+
+
+@ROUTES.route('', methods=['GET'])
+def locations_get():
+    if request.args.has_key('id') and request.args.has_key('planid'):
+        # both id and planid exist, doesn't make sense
+        return '400: Too many arguments specified'
+    if request.args.has_key('id'):
+        # We know id exists, try to cast to int
+        id = request.args.get('id', type=int)
+        # If ID cannot be cast to an it, id=None
+        if not id:
+            # id was not a number, complain accordingly
+            return '400: Argument id is not an integer'
+        if id < 0:
+            # id must be positive integer
+            return '400: Argument id is not positive'
+        # We are now happy the id is good
+        # Do db tings
+        return 'Location with id {}'.format(id)
+
+    if request.args.has_key('planid'):
+        # We know planid exists, try to cast to int
+        planid = request.args.get('planid', type=int)
+        # If plan ID cannot be cast to an it, id=None
+        if not planid:
+            # id was not a number, complain accordingly
+            return '400: Argument planid is not an integer'
+        if planid < 0:
+            # id must be positive integer
+            return '400: Argument planid is not positive'
+        # We are now happy the id is good
+        # Do db tings
+        return 'List of locations with planid {}'.format(planid)
+
+    # No filter specified, REST would return everything but we just error
+    return '400: Location resource id or planid not specified'
--- api/plans.py
+++ api/plans.py
@@ -1,13 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-ROUTES = Blueprint('plans')
-
-
-@ROUTES.route("/")
-def plans_all():
-    return "List of all plans"
+ROUTES = Blueprint('plans', __name__)
 
 
-@ROUTES.route("/id/<planid>")
-def plan_from_id(planid):
-    return "Plan with id: {}".format(planid)
+@ROUTES.route('', methods=['GET'])
+def plans_get():
+    # /api/plans?id=<id>
+    if request.args.has_key('id'):
+        # We know id exists, try to cast to int
+        id = request.args.get('id', type=int)
+        # If ID cannot be cast to an it, id=None
+        if not id:
+            # id was not a number, complain accordingly
+            return '400: Argument id is not an integer'
+        if id < 0:
+            # id must be positive integer
+            return '400: Argument id is not positive'
+        # We are now happy the id is good
+        # Do db tings
+        return 'Plan with id {}'.format(id)
+
+    # No filter specified, REST would return everything but we just error
+    return '400: Plan resource id not specified'
