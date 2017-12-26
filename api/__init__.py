2ec592a6e70e68b4b78f7f06b6895cf1dca7212f
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-26 22:20:55 +0000

    Jsonify decorator
    * move register blueprints function

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import plans, events, routes
-
-def register_api_blueprints(app):
-    app.register_blueprint(plans.ROUTES, url_prefix='/api/plans')
-    app.register_blueprint(events.ROUTES, url_prefix='/api/events')
-    app.register_blueprint(routes.ROUTES, url_prefix='/api/routes')
--- back_end/__init__.py
+++ back_end/__init__.py
@@ -1,4 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+import plans, events, routes
+from flask import jsonify
+
+
+def register_api_blueprints(app):
+    app.register_blueprint(plans.ROUTES, url_prefix='/api/plans')
+    app.register_blueprint(events.ROUTES, url_prefix='/api/events')
+    app.register_blueprint(routes.ROUTES, url_prefix='/api/routes')
+
+
+def jsonify_decorator(func):
+    def wrapper(*args, **kwargs):
+        json = kwargs.pop('json', True)
+        obj = func(*args, **kwargs)
+        if json:
+            return jsonify(obj.serialise)  # TODO serialise property
+        return obj
+    return wrapper
+
+
 
 def init(app):
     global db
@@ -6,5 +26,19 @@ def init(app):
     import plans, events, routes, route_events
     db.create_all()
 
+
 def reset():
     db.drop_all()
+
+
+if __name__ == '__main__':
+    # Fake jsonify for testing
+    def jsonify(s):
+        return 'JSON:' + s
+
+    @jsonify_decorator
+    def test_func(s):
+        return s + ", test!"
+
+    print test_func('lol')
+    print test_func('lol2', json=False)
