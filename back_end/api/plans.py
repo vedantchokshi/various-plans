4f21e87dfc9e2f24df8b5cf83058a4295181f54a
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-28 16:59:57 +0000

    API doesn't do validation

--- back_end/api/plans.py
+++ back_end/api/plans.py
@@ -8,8 +8,4 @@ ROUTES = Blueprint('plan', __name__)
 @ROUTES.route('/<planid>', methods=['GET'])
 @jsonify_decorator
 def plans_get(planid):
-    plan = db.plans.get_from_id(planid)
-    if not plan:
-        return 'No plan with id: ' + str(planid)
-    else:
-        return plan
+    return db.plans.get_from_id(planid)
