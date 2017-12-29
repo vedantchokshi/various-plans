21520106bea956b131c405aec2b5882915ca1cca
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-29 00:59:04 +0000

    Add status code to jsonify_decorator

--- back_end/api/__init__.py
+++ back_end/api/__init__.py
@@ -1,5 +1,6 @@
-from flask import jsonify
 from functools import wraps
+
+from flask import jsonify, make_response
 
 
 def init(app):
@@ -13,11 +14,11 @@ def jsonify_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         json = kwargs.pop('json', True)
-        obj = func(*args, **kwargs)
+        obj, status_code = func(*args, **kwargs)
         if json:
             if isinstance(obj, list):
-                return jsonify(results=[i.serialise for i in obj])
-            return jsonify(obj.serialise)  # TODO serialise property
+                return make_response(jsonify(results=[i.serialise for i in obj]), status_code)
+            return make_response(jsonify(obj.serialise), status_code)
         return obj
 
     return wrapper
