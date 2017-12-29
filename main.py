4e2a3f5a2e7fd02be56d63358f572693860255aa
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-29 02:44:34 +0000

    Tidy main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# [START imports]
-import logging, config
-from flask import Flask, render_template, request, redirect, url_for
-from back_end.forms import PlanForm, EventForm, RouteForm
+import logging
+
+from flask import Flask, render_template
+
 import back_end as be
+import config
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -10,15 +11,19 @@ app.debug = True
 
 be.init(app)
 
+from back_end import db
+from back_end.db import plans
+
 
 # TDEV reset
 @app.route('/reset', methods=['GET'])
 def reset():
     if not app.debug:
-        #TODO throw error
+        # TODO throw error
         return 'Nope :)'
-    be.db.reset(app)
+    be.db.reset()
     return 'Reset!'
+
 
 # [homepage]
 @app.route('/', methods=['GET'])
@@ -28,48 +33,10 @@ def index():
 
 
 # [plan view]
-@app.route('/plan/<planid>', methods=['GET'])
+@app.route('/#/<planid>', methods=['GET'])
 def disp_plan(planid):
     plan = be.db.plans.get_from_id(planid)
     return render_template('plan.html', plan=plan)
-
-
-# [new plan]
-@app.route('/plan/new', methods=['POST', 'GET'])
-def new_plan():
-    if (request.method == "GET"):
-        form = PlanForm()
-        return render_template('new_plan.html', form=form)
-    elif (request.method == 'POST'):
-        # create new plan in db
-        newPlan = be.db.plans.create(request.form['name'])
-        return redirect(url_for('disp_plan', planid=newPlan.id))
-
-
-# [new event]
-@app.route('/plan/<planid>/event/new', methods=['POST', 'GET'])
-def new_event(planid):
-    if (request.method == "GET"):
-        form = EventForm()
-        plan = be.db.plans.get_from_id(planid)
-        return render_template('new_event.html', form=form, plan=plan)
-
-    elif (request.method == 'POST'):
-        e = be.db.events.create(planid, request.form['name'], request.form['location'])
-        return redirect(url_for('disp_plan', planid=e.plan.id))
-
-
-# [new route]
-@app.route('/plan/<planid>/route/new', methods=['POST', 'GET'])
-def new_route(planid):
-    if (request.method == "GET"):
-        form = RouteForm()
-        plan = be.db.plans.get_from_id(planid)
-        return render_template('new_route.html', form=form, plan=plan)
-    elif (request.method == 'POST'):
-        r = be.db.routes.create(planid,request.form['name'],list(map(int, (request.form['eventids']).split(','))))
-        return redirect(url_for('disp_plan', planid=r.plan.id))
-
 
 
 @app.errorhandler(500)
@@ -77,40 +44,3 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-
-
-
-
-# TODO remove all below functions
-
-# [count votes]
-@app.route('/plan/<planid>/countvotes', methods=['POST'])
-def countvotes(planid):
-    plan = be.db.plans.countvotes(planid)
-    return redirect(url_for('disp_plan', planid=plan.id))
-
-
-# [vote]
-@app.route('/event/<eventid>/upvote', methods=['POST'])
-def upvote_event(eventid):
-    e = be.db.events.upvote(eventid)
-    return redirect(url_for('disp_plan', planid=e.planid))
-
-
-@app.route('/event/<eventid>/downvote', methods=['POST'])
-def downvote_event(eventid):
-    e = be.db.events.downvote(eventid)
-    return redirect(url_for('disp_plan', planid=e.planid))
-
-
-@app.route('/route/<routeid>/upvote', methods=['POST'])
-def upvote_route(routeid):
-    r = be.db.routes.upvote(routeid)
-    return redirect(url_for('disp_plan', planid=r.planid))
-
-
-@app.route('/route/<routeid>/downvote', methods=['POST'])
-def downvote_route(routeid):
-    r = be.db.routes.downvote(routeid)
-    return redirect(url_for('disp_plan', planid=r.planid))
-
