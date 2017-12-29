cee44e4dbd1809d1ec34670bd2374d2b34a6227c
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-29 01:59:19 +0000

    Fix annoying pycharm error
    * if backend isn't init, will give a more informative error

--- back_end/db/__init__.py
+++ back_end/db/__init__.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 # MUST reset the db, using reset() after changing default_str_len
 default_str_len = 100
 
+db = SQLAlchemy()
+
+
 def init(app):
     global db
     db = SQLAlchemy(app)
     import plans, events, routes, route_events
+
 
 def reset(app):
     db.drop_all()
