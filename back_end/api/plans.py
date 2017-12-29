833225699474ebb020127d999b7efcc1bec0883b
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-29 01:52:48 +0000

    Fix api plan url status codes and validation

--- back_end/api/plans.py
+++ back_end/api/plans.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
-from . import jsonify_decorator
-from .. import db
+
+from back_end.api import jsonify_decorator
+from back_end.api.api_exceptions import InvalidContent
+from back_end.db import plans
 
 ROUTES = Blueprint('plan', __name__)
 
@@ -8,25 +10,26 @@ ROUTES = Blueprint('plan', __name__)
 @ROUTES.route('/<planid>', methods=['GET'])
 @jsonify_decorator
 def get_plan(planid):
-    return db.plans.get_from_id(planid)
+    return plans.get_from_id(planid), 200
 
 
 @ROUTES.route('/<planid>/events', methods=['GET'])
 @jsonify_decorator
 def get_events_from_plan(planid):
-    return db.plans.get_from_id(planid).events
+    return plans.get_from_id(planid).events, 200
 
 
 @ROUTES.route('/<planid>/routes', methods=['GET'])
 @jsonify_decorator
 def get_routes_from_plan(planid):
-    return db.plans.get_from_id(planid).routes
+    return plans.get_from_id(planid).routes, 200
 
 
 @ROUTES.route('', methods=['POST'])
 @jsonify_decorator
 def create_plan():
     json = request.get_json()
-    # If keys don't exist, None is used
-    return db.plans.create(json.get('name'), json.get('eventVoteCloseTime'), json.get('routeVoteCloseTime'),
-                           json.get('startTime'), json.get('endTime'))
+    if json is None:
+        raise InvalidContent("Empty json not a valid plan object")
+    return plans.create(json.get('name'), json.get('eventVoteCloseTime'), json.get('routeVoteCloseTime'),
+                           json.get('startTime'), json.get('endTime')), 201
