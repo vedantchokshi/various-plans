c09fd232220437d99efb3f3e420a283373a190c3
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-30 18:58:51 +0000

    Filter routes and fix route creation validation

--- back_end/db/routes.py
+++ back_end/db/routes.py
@@ -1,8 +1,8 @@
-import events
-import plans
-import route_events
-from back_end.db import db, default_str_len
+from back_end.db import db, default_str_len, plans, events, route_events
 from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent
+
+plans.Plan.routes = property(
+    lambda self: self.routes_all.order_by(Route.votes.desc())[0:1] if self.phase > 2 else self.routes_all)
 
 
 class Route(db.Model):
@@ -12,7 +12,7 @@ class Route(db.Model):
     planid = db.Column(db.Integer, db.ForeignKey('Plans.id'), nullable=False)
     votes = db.Column(db.Integer, nullable=False, default=0)
 
-    plan = db.relationship('Plan', backref=db.backref('routes', lazy=True))
+    plan = db.relationship('Plan', backref=db.backref('routes_all', lazy='dynamic'))
     events = db.relationship('Event', secondary='route_event')
 
     def __init__(self, name):
@@ -43,20 +43,25 @@ def get_from_id(routeid):
 def create(planid, name, eventidList):
     if name is None or not name:
         raise InvalidContent('Route name is not specified')
-    if eventidList is None or len(eventidList) == 0:
-        raise InvalidContent('Route event list must have a non-zero size')
+    if eventidList is None:
+        raise InvalidContent('Route eventidList is not specified')
+    if len(eventidList) == 0:
+        raise InvalidContent('Route eventidList must have a non-zero size')
     if len(set(eventidList)) != len(eventidList):
-        raise InvalidContent('Route cannot repeat an event')
+        raise InvalidContent('Route eventidList cannot repeat an event')
 
     # Finding a plan will check the validity of planid
     plan = plans.get_from_id(planid)
 
-    for eventid in eventidList:
-        if events.get_from_id(eventid).planid != int(planid):
-            raise InvalidContent("Event '{}' is not in Plan '{}'".format(eventid, planid))
-
     if plan.phase != 2:
         raise InvalidRequest("Plan '{}' is not in phase 2".format(planid))
+
+    for eventid in eventidList:
+        event = events.get_from_id(eventid)
+        if event.planid != plan.planid:
+            raise InvalidContent("Event '{}' is not in Plan '{}'".format(event.id, plan.planid))
+        if event not in plan.events:
+            raise InvalidContent("Event '{}' does not have enough votes".format(event.id))
 
     new_route = Route(name)
 
