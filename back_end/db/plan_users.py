d5197e59c6dd10a030329f65d403921d56e7db37
Mark Tuddenham <mark@tudders.com>
2018-01-01 18:12:08 +0000

    added backrefs

--- back_end/db/plan_users.py
+++ back_end/db/plan_users.py
@@ -1,10 +1,12 @@
 from back_end.db import db, default_str_len
 
 
-class RouteEvent(db.Model):
+class PlanUser(db.Model):
     __tablename__ = 'Plan_Users'
     planid = db.Column(db.Integer, db.ForeignKey('Plans.id'), primary_key=True)
     userid = db.Column(db.String(default_str_len), primary_key=True)
+
+    plan = db.relationship('Plan', backref=db.backref('users', lazy=True))
 
     def __init__(self, planid, userid):
         self.palnid = planid
--- back_end/db/votes.py
+++ back_end/db/votes.py
@@ -1,5 +1,19 @@
+from back_end.db import routes, events
 from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent
 from back_end.db import db, default_str_len
+
+
+events.Event.vote = 
+    lambda self, userid, vote: _set_event_vote(self.id, userid, vote)
+
+routes.Route.vote =
+    lambda self, userid, vote: _set_route_vote(self.id, userid, vote)
+
+events.Event.vote_count = property(
+    lambda self: sum(e.vote for e in EventVote.query.filter_by(eventid=self.id)))
+
+routes.Route.vote_count = property(
+    lambda self: sum(r.vote for r in RouteVote.query.filter_by(routeid=self.id)))
 
 
 class EventVote(db.Model):
@@ -7,6 +21,8 @@ class EventVote(db.Model):
     eventid = db.Column(db.Integer, db.ForeignKey('Events.id'), primary_key=True)
     userid = db.Column(db.String(default_str_len), nullable=False, primary_key=True)
     vote = db.Column(db.Integer, nullable=False)
+
+    event = db.relationship('Event', backref=db.backref('votes', lazy='dynamic'))
 
     def __init__(self, eventid, userid, vote):
         self.eventid = eventid
@@ -25,6 +41,8 @@ class RouteVote(db.Model):
     userid = db.Column(db.String(default_str_len), nullable=False, primary_key=True)
     vote = db.Column(db.Integer, nullable=False)
 
+    route = db.relationship('Route', backref=db.backref('votes', lazy='dynamic'))
+
     def __init__(self, routeid, userid, vote):
         self.routeid = routeid
         self.userid = userid
@@ -36,19 +54,19 @@ class RouteVote(db.Model):
         return s
 
 
-def get_event_vote(eventid, userid):
+def get_event_votes(userid):
     try:
-        return (EventVote.query.filter_by(eventid=int(eventid), userid=userid)).vote
+        return EventVote.query.filter_by(userid=userid)
     except ValueError:
         return None
 
-def get_route_vote(routeid, userid):
+def get_route_votes(userid):
         try:
-            return (RouteVote.query.filter_by(routeid=int(routeid), userid=userid)).vote
+            return RouteVote.query.filter_by(userid=userid)
         except ValueError:
             return None
 
-def set_event_vote(eventid, userid, vote):
+def _set_event_vote(eventid, userid, vote):
     try:
         ev = EventVote.query.filter_by(eventid=int(eventid), userid=userid)
         if not ev:
@@ -61,9 +79,9 @@ def set_event_vote(eventid, userid, vote):
     except ValueError:
         return None
 
-def set_route_vote(routeid, userid, vote):
+def _set_route_vote(routeid, userid, vote):
         try:
-            rv RouteVote.query.filter_by(routeid=int(routeid), userid=userid)
+            rv = RouteVote.query.filter_by(routeid=int(routeid), userid=userid)
         if not rv:
             rv = RouteVote(routeid, userid, vote)
             db.session.add(rv)
