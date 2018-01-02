8c45288d1d08d65ce31b07552bba7399fcc32342
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-02 19:12:13 +0000

    Remove authenticate_user_plan function, tidy db/__init__.py

--- back_end/db/__init__.py
+++ back_end/db/__init__.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# MUST reset the db, using reset() after changing default_str_len
 default_str_len = 100
 
 db = SQLAlchemy()
@@ -17,10 +16,3 @@ def reset():
     db.drop_all()
     import plans, events, routes, route_events
     db.create_all()
-
-
-def authenticate_user_plan(planid, userid):
-    # AUTHTODO check that in the userplan table there is a record with userplan.userid=userid and userplan.planid=planid
-    # May have to return all records where the userplan.userid=userid and then check one has a planid of planid. (cos objects)
-    # return true or false depending on result
-    return True
