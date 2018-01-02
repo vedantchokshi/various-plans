e997a314d33469de042cfda35d8cdb973c19860a
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-02 17:02:38 +0000

    Tidy votes.py

--- back_end/db/votes.py
+++ back_end/db/votes.py
@@ -2,18 +2,13 @@ from back_end.db import routes, events
 from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent
 from back_end.db import db, default_str_len
 
+events.Event.vote = lambda self, userid, vote: _set_event_vote(self.id, userid, vote)
 
-events.Event.vote = 
-    lambda self, userid, vote: _set_event_vote(self.id, userid, vote)
+routes.Route.vote = lambda self, userid, vote: _set_route_vote(self.id, userid, vote)
 
-routes.Route.vote =
-    lambda self, userid, vote: _set_route_vote(self.id, userid, vote)
+events.Event.vote_count = property(lambda self: sum(e.vote for e in EventVote.query.filter_by(eventid=self.id)))
 
-events.Event.vote_count = property(
-    lambda self: sum(e.vote for e in EventVote.query.filter_by(eventid=self.id)))
-
-routes.Route.vote_count = property(
-    lambda self: sum(r.vote for r in RouteVote.query.filter_by(routeid=self.id)))
+routes.Route.vote_count = property(lambda self: sum(r.vote for r in RouteVote.query.filter_by(routeid=self.id)))
 
 
 class EventVote(db.Model):
@@ -60,11 +55,13 @@ def get_event_votes(userid):
     except ValueError:
         return None
 
+
 def get_route_votes(userid):
-        try:
-            return RouteVote.query.filter_by(userid=userid)
-        except ValueError:
-            return None
+    try:
+        return RouteVote.query.filter_by(userid=userid)
+    except ValueError:
+        return None
+
 
 def _set_event_vote(eventid, userid, vote):
     try:
@@ -79,9 +76,10 @@ def _set_event_vote(eventid, userid, vote):
     except ValueError:
         return None
 
+
 def _set_route_vote(routeid, userid, vote):
-        try:
-            rv = RouteVote.query.filter_by(routeid=int(routeid), userid=userid)
+    try:
+        rv = RouteVote.query.filter_by(routeid=int(routeid), userid=userid)
         if not rv:
             rv = RouteVote(routeid, userid, vote)
             db.session.add(rv)
@@ -89,6 +87,6 @@ def _set_route_vote(routeid, userid, vote):
             rv.vote = vote
 
         db.session.commit()
+
     except ValueError:
         return None
-
