741d398fc3d8ad62c5ad79a816ae35bd0f87bdd2
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-03 01:37:53 +0000

    Add content attribute to exception for extra information in json

--- back_end/exceptions.py
+++ back_end/exceptions.py
@@ -1,17 +1,23 @@
 class BaseApiException(Exception):
     status_code = 500
+    content = {}
 
-    def __init__(self, message, status_code=None):
+    def __init__(self, message, status_code=None, content=None):
         Exception.__init__(self)
         self.message = message
         if status_code is not None:
             self.status_code = status_code
+        if content is not None:
+            self.content = content
 
     @property
     def serialise(self):
         rv = dict()
         rv['message'] = self.message
         rv['status_code'] = self.status_code
+        if self.content is not None:
+            for key, value in self.content.items:
+                rv[key] = value
         return rv
 
 
