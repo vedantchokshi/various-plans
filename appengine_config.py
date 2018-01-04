7b05195aa80ffef36d3f295cfeb97f31c2ebdfdc
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-04 01:53:18 +0000

    Use explicit uri for lib folder
    * should work with more OSs

--- appengine_config.py
+++ appengine_config.py
@@ -19,7 +19,7 @@ import os
 # [START vendor]
 from google.appengine.ext import vendor
 
-vendor.add('lib')
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
 if os.environ.get('GAE_ENV'):
     print('GAE instance! (appengine config)')
