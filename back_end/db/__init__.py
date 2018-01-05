455fb6726243482fa669d812a2844857eb19017d
Mark Tuddenham <mark@tudders.com>
2018-01-05 17:38:44 +0000

    tidy __init__.py

--- back_end/db/__init__.py
+++ back_end/db/__init__.py
@@ -2,26 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 default_str_len = 100
 
-db = SQLAlchemy()
-
-
 def init(app):
     global db
     db = SQLAlchemy(app)
     import_all()
 
-
 def reset():
-    global db
     db.drop_all()
     import_all()
     db.create_all()
 
-
 def import_all():
-    import plans
-    import events
-    import routes
-    import route_events
-    import votes
-    import plan_users
+    import plans, events, routes, route_events, votes, plan_users
