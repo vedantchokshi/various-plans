2ef6ace078067c92b1e9bab34b71d851463658ea
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-05 21:40:54 +0000

    Fix no variable db in backend.db

--- back_end/db/__init__.py
+++ back_end/db/__init__.py
@@ -2,15 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 default_str_len = 100
 
+# db variable should exist from the start
+# has helped debug in the past
+db = SQLAlchemy()
+
+
 def init(app):
     global db
     db = SQLAlchemy(app)
     import_all()
 
+
 def reset():
     db.drop_all()
     import_all()
     db.create_all()
+
 
 def import_all():
     import plans, events, routes, route_events, votes, plan_users
