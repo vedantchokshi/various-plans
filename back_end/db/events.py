99c0a40f70aaa01cb35db31c5c82544321002405
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-06 17:50:38 +0000

    Sort events correctly
    * includes plan phase login

--- back_end/db/events.py
+++ back_end/db/events.py
@@ -1,5 +1,46 @@
 from back_end.db import db, default_str_len, plans
-from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent
+from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent, BaseApiException
+
+
+def get_events(plan):
+    events = plan.events_all.all()
+    if len(events) > 0:
+        # Sort events based on our criteria
+        events = sort_events(events)
+        if plan.phase < 2:
+            # Phase 1 requires no filtering of events
+            return events
+        # Phases 2, 3, 4 require positively voted events
+        events = [event for event in events if event.votes > 0]
+        if plan.phase < 3:
+            # Phase 2 requires no more filtering of events
+            return events
+        # Phases 3 and 4 require only events from winning route
+        routes = plan.routes
+        if len(routes) > 0:
+            # Filter events if there is a winning route
+            route = routes[0]
+            return [event for event in events if event.route.id == route.id]
+    return []
+
+
+def sort_events(event_list):
+    def sorter(a, b):
+        if not isinstance(a, Event) or not isinstance(b, Event):
+            raise BaseApiException('Routes list does not contain route objects', 500)
+        if a.votes != b.votes:
+            # Highest total votes
+            return cmp(b.votes, a.votes)
+        a_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), a.all_votes.all())
+        b_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), b.all_votes.all())
+        if a_downvotes != b_downvotes:
+            # Least down-voted route
+            return cmp(a_downvotes, b_downvotes)
+        # Order of creation
+        return cmp(a.id, b.id)
+
+    return sorted(event_list, cmp=sorter)
+
 
 plans.Plan.events = property(
     lambda self: [x for x in self.events_all.all() if x.votes > 0] if self.phase > 1 else self.events_all.all())
