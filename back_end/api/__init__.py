619ecbdfacb36403f52ba7a4ae377a47ed685d26
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-07 20:31:53 +0000

    [pylint 10/10] api/__init__.py
    * delete api/token.py, not used anymore

--- back_end/api/__init__.py
+++ back_end/api/__init__.py
@@ -1,3 +1,6 @@
+"""
+Initialise API function and useful function decorators
+"""
 import time
 from functools import wraps
 
@@ -7,50 +10,57 @@ from google.oauth2 import id_token
 
 from back_end.exceptions import Unauthorized
 
-debug = False
-
 
 def init(app, prefix):
-    global debug
-    import plans
-    import events
-    import routes
-    import users
-    import token
-    debug = app.debug
+    """
+    Initialise the app with the blueprints defined in this module
+
+    :param app: Flask application
+    :param prefix: URL prefix for all API URLs
+    """
+    # pylint: disable-msg=cyclic-import
+    # Imports are safe as they are called inside this function
+    import back_end.api.plans as plans
+    import back_end.api.events as events
+    import back_end.api.routes as routes
+    import back_end.api.users as users
     app.register_blueprint(plans.ROUTES, url_prefix='{}/plan'.format(prefix))
     app.register_blueprint(events.ROUTES, url_prefix='{}/event'.format(prefix))
     app.register_blueprint(routes.ROUTES, url_prefix='{}/route'.format(prefix))
     app.register_blueprint(users.ROUTES, url_prefix='{}/user'.format(prefix))
-    # TODO get rid of this and token.py
-    app.register_blueprint(token.ROUTES, url_prefix='{}/token'.format(prefix))
 
 
 def get_userid_from_token(token):
+    """
+    Use Google oauth2 service to verify a Google signin token
+
+    :return: Google Auth user ID
+    :rtype: str
+    """
     try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(),
-                                              "952476275187-ef3icj10cn4ptsl3ehs3jcg3tdeff0pv.apps.googleusercontent.com"
-                                              )
+        idinfo = id_token.verify_oauth2_token(
+            token, requests.Request(),
+            "952476275187-ef3icj10cn4ptsl3ehs3jcg3tdeff0pv.apps.googleusercontent.com"
+        )
 
         if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
             raise ValueError('Wrong issuer.')
 
-        # If auth request is from a G Suite domain:
-        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-        #     raise ValueError('Wrong hosted domain.')
-
         # ID token is valid. Get the user's Google Account ID from the decoded token.
         return idinfo['sub']
-
-        # print('userid: {}'.format(idinfo['sub']), file=sys.stderr)
-        # print('user name: {}'.format(idinfo['name']), file=sys.stderr)
     except ValueError:
-        raise Unauthorized('Please log in')
+        raise Unauthorized('Invalid token')
 
 
 def token_decorator(func):
+    """
+    Get the Google signin token from the request cookie `vp-token` and
+    pass it as the decorated function as the argument `userid`
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
+        # pylint: disable-msg=missing-docstring
+        # @wraps takes the docstring from the wrapped function
         token = request.cookies.get('vp-token')
         if token is not None:
             userid = get_userid_from_token(token)
@@ -62,16 +72,26 @@ def token_decorator(func):
 
 
 def jsonify_decorator(func):
+    """
+    Take the result of the decorated function and create a json response.
+
+    Functions must return a tuple of `(object, status_code)`.
+
+    The object may be a list of objects but all objects must implement `.serialise`
+    which returns a dictionary representation of themselves.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
+        # pylint: disable-msg=missing-docstring
+        # @wraps takes the docstring from the wrapped function
         timestamp = time.time()
         json = kwargs.pop('json', True)
         obj, status_code = func(*args, **kwargs)
         if json:
             if isinstance(obj, list):
-                d = dict()
-                d['results'] = [i.serialise for i in obj]
-                obj = d
+                _d = dict()
+                _d['results'] = [i.serialise for i in obj]
+                obj = _d
             else:
                 obj = obj.serialise
             obj['timestamp'] = timestamp
