0fd4a1fd55d86700f5b27aa7f4b5c3841e82536f
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-07 18:58:32 +0000

    Comment unused functions in db/events.py

--- back_end/db/events.py
+++ back_end/db/events.py
@@ -4,28 +4,46 @@ from back_end.db import db, default_str_len, plans
 from back_end.exceptions import InvalidRequest, ResourceNotFound, InvalidContent, BaseApiException
 
 
-def get_events(plan):
-    events = plan.events_all.all()
-    if len(events) > 0:
-        # Sort events based on our criteria
-        # events = sort_events(events)
-        if plan.timephase < 2:
-            # Phase 1 requires no filtering of events
-            return events
-        # Phases 2, 3, 4 require positively voted events
-        events = [event for event in events if event.votes > 0]
-        if plan.timephase < 3:
-            # Phase 2 requires no more filtering of events
-            return events
-        # Phases 3 and 4 require only events from winning route
-        routes = plan.routes
-        if len(routes) > 0:
-            # Filter events if there is a winning route
-            route = routes[0]
-            route_event_ids = [event.id for event in route.events]
-            events = [event for event in events if event.id in route_event_ids]
-            return events
-    return []
+# def get_events(plan):
+#     events = plan.events_all.all()
+#     if len(events) > 0:
+#         # Sort events based on our criteria
+#         # events = sort_events(events)
+#         if plan.timephase < 2:
+#             # Phase 1 requires no filtering of events
+#             return events
+#         # Phases 2, 3, 4 require positively voted events
+#         events = [event for event in events if event.votes > 0]
+#         if plan.timephase < 3:
+#             # Phase 2 requires no more filtering of events
+#             return events
+#         # Phases 3 and 4 require only events from winning route
+#         routes = plan.routes
+#         if len(routes) > 0:
+#             # Filter events if there is a winning route
+#             route = routes[0]
+#             route_event_ids = [event.id for event in route.events]
+#             events = [event for event in events if event.id in route_event_ids]
+#             return events
+#     return []
+#
+#
+# def sort_events(event_list):
+#     def sorter(a, b):
+#         if not isinstance(a, Event) or not isinstance(b, Event):
+#             raise BaseApiException('Routes list does not contain route objects', 500)
+#         if a.votes != b.votes:
+#             # Highest total votes
+#             return cmp(b.votes, a.votes)
+#         a_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), a.all_votes.all(), 0)
+#         b_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), b.all_votes.all(), 0)
+#         if a_downvotes != b_downvotes:
+#             # Least down-voted route
+#             return cmp(a_downvotes, b_downvotes)
+#         # Order of creation
+#         return cmp(a.id, b.id)
+#
+#     return sorted(event_list, cmp=sorter)
 
 
 def get_events_sql(plan):
@@ -38,24 +56,6 @@ def get_events_sql(plan):
 
 def count_positive_events_sql(plan):
     return plan.events_all.filter(Event.votes > 0).count()
-
-
-def sort_events(event_list):
-    def sorter(a, b):
-        if not isinstance(a, Event) or not isinstance(b, Event):
-            raise BaseApiException('Routes list does not contain route objects', 500)
-        if a.votes != b.votes:
-            # Highest total votes
-            return cmp(b.votes, a.votes)
-        a_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), a.all_votes.all(), 0)
-        b_downvotes = reduce(lambda x, y: x + (1 if y.vote < 1 else 0), b.all_votes.all(), 0)
-        if a_downvotes != b_downvotes:
-            # Least down-voted route
-            return cmp(a_downvotes, b_downvotes)
-        # Order of creation
-        return cmp(a.id, b.id)
-
-    return sorted(event_list, cmp=sorter)
 
 
 plans.Plan.events = property(get_events_sql)
