80fdd2c0cd1858c489129a93c2baa1a8b0b16e96
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-01-08 02:12:40 +0000

    [pylint 10/10][db/plan_users.py] Add docstring

--- back_end/db/plan_users.py
+++ back_end/db/plan_users.py
@@ -1,7 +1,14 @@
+"""
+PlanUser object and user functions
+"""
 from back_end.db import DB, STR_LEN
 
 
 class PlanUser(DB.Model):
+    # pylint: disable-msg=too-few-public-methods
+    """
+    PlanUser object that links users to plans]
+    """
     __tablename__ = 'Plan_Users'
     planid = DB.Column(DB.Integer, DB.ForeignKey('Plans.id'), primary_key=True)
     userid = DB.Column(DB.String(STR_LEN), primary_key=True)
@@ -14,5 +21,8 @@ class PlanUser(DB.Model):
 
 
 def get_plans(userid):
+    """
+    Get all the plans that a user is associated with
+    """
     plans = PlanUser.query.filter_by(userid=userid)
     return [p.plan for p in plans]
